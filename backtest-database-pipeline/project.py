c5cd540245121b878775615f43e65238dfadc700
waltertang27 <90427972+waltertang27@users.noreply.github.com>
2022-01-07 21:14:19 -0800

    added webserver using flask

--- backtest-database-pipeline/project.py
+++ backtest-database-pipeline/project.py
@@ -3,6 +3,8 @@ import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 from polygon import RESTClient
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 
@@ -17,6 +19,10 @@ password = "password"
 engine = create_engine('postgresql://postgres:{}@localhost/Project_Data'.format(password))
 #temp_data = 'backtest-database-pipeline'
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/Project_Data'
+db = SQLAlchemy(app)
+
 
 
 def create_table(symbol):
@@ -30,5 +36,34 @@ def create_table(symbol):
 
 
 create_table('APPL')
+
+class listing(db.Model):
+    __tablename__ = 'listings'
+    v = db.Column(db.Float, primary_key = True)
+    vw = db.Column(db.Float)
+    o = db.Column(db.Float)
+    c = db.Column(db.Float)
+    h = db.Column(db.Float)
+    l = db.Column(db.Float)
+    t = db.Column(db.DateTime)
+    n = db.Column(db.BigInteger)
+    
+def __init__(self, v, vw, o, c, h, l, t, n):
+    self.v = v
+    self.vw = vw
+    self.o = o
+    self.c = c
+    self.h = h
+    self.l = l
+    self.t = t
+    self.n = n
+
+@app.route('/test', methods = ['GET'])
+def test():
+    return {
+        'test': 'testing'
+    }
+db.create_all()
+app.run()
 
 
