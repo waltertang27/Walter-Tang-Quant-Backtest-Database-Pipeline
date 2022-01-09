ee18717001185ed6586e36bebb2622080efd6188
waltertang27 <90427972+waltertang27@users.noreply.github.com>
2022-01-08 17:45:45 -0800

    code cleanup

--- backtest-database-pipeline/project.py
+++ backtest-database-pipeline/project.py
@@ -10,24 +10,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 #importing the polygon client. 
-# reads the data into a csv file (in this case, APPL wit one hour timespan from 12/1/2021 to 12/30/2021)
+
 key = "ozggTSfotBRqphmVTTL3tpakMp07rHMl"
 client = RESTClient(key)
+# reads the data from Polygon.io into a csv file (in this case, APPL wit one hour timespan from 12/1/2021 to 12/30/2021)
 response = client.stocks_equities_aggregates('AAPL', multiplier = 1, timespan = 'hour', from_ = '2021-12-01', to = '2021-12-30')
 data_frame = pd.DataFrame(response.results)
-#name of csv file to be created
-data_frame.to_csv('data/APPL.csv')
+
+#converts to csv file. path contains name of csv file to be created
+data_frame.to_csv('data/APPL.csv', index = False)
+#password for PgAdmin4
 password = "password"
 engine = create_engine('postgresql://postgres:{}@localhost/Project_Data'.format(password))
 
+#creates Flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MOOIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/Project_Data'
-#app.debug = True
 db = SQLAlchemy(app)
 
 
-#creates the data table and sends it into the database
+#creates the data table and sends to database
 def create_table(symbol):
     df = pd.read_csv('{}/{}.csv'.format('data', symbol))
     #v,vw,o,c,h,l,t,n   
@@ -35,9 +38,10 @@ def create_table(symbol):
     df['t'] = pd.to_datetime(df['t'])
     df = df.fillna(0)
 
+    #sends to "prices" database
     df.to_sql('prices', engine, if_exists = 'replace', index = False)
 
-#creates table for APPL csv file
+#creates table for APPL csv file that we pulled earlier from polygon.io
 create_table('APPL')
 
 #class for intializing object for webserver
@@ -63,12 +67,15 @@ class listing(db.Model):
         self.n = n
 
 
-#put link that is given in postman, and add /test at the end
+#TESTING: put link that is given in postman, and add /test at the end
 @app.route('/test', methods = ['GET'])
 def test():
     return {
         'test': 'testing'
     }
+
+#used for posting/getting values to/from database
+
 #put link that is given in postman, then add /list at the end
 @app.route('/list', methods = ['GET'])
 #get method; returns the current listings in the database
